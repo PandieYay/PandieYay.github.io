c07cd40724d07f3975a9903e2c48bc1b77af9065
Edison Lim <babyedison@gmail.com>
2021-10-01 21:06:29 +0800

    Added Password

--- event-handler/js/events.js
+++ event-handler/js/events.js
@@ -2,7 +2,23 @@ function transformUppercase(_val){
     return _val.toUpperCase();
 }
 
-let elNamaPengguna = document.getElementById("namaPenggunga")
-elNamaPengguna.addEventListener("keyup", function(){
-    elNamaPengguna.value = transformUppercase(elNamaPengguna.value)
+// DOM event listener
+let ElUsername = document.getElementById("Username")
+ElUsername.addEventListener("keyup", function(){
+    ElUsername.value = transformUppercase(ElUsername.value)
 })
+
+function checkPassword(){
+    let ElPassword = document.getElementById("Password")
+    let pass_val = ElPassword.value
+
+    if(pass_val.length > 5){
+        alert("Password check...OK!")
+    } else {
+        alert("Password too short!")
+    }
+}
+
+// Traditional DOM event handler
+let elKL = document.getElementById("Username")
+elKL.onblur = checkPassword
